c0cdddd1e3b671484951dcae9f09658375a3e11b
Josef Pospíšil <josef.pospisil@laststar.eu>
2014-10-24 16:50:49 +0200

    [Experimental] Add macro for router definition
    Experimental compile time crate loading have to be enabled for nickel
    macros to be loaded. Still it seems much nicer and calls for
    refactoring. Not for public consumation I guess.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+#![feature(phase)]
+#[phase(plugin)] extern crate nickel_macros;
+
 extern crate nickel;
 extern crate postgres;
 extern crate serialize;
@@ -35,50 +38,51 @@ impl ToJson for Vec<Todo> {
 fn main() {
     let mut server = Nickel::new();
 
+    let router = router! {
+        get "/todos" => |request, response| {
+            let conn = PostgresConnection::connect("postgres://rustmvc@localhost",
+                                                   &NoSsl).unwrap();
+
+            let stmt = conn.prepare("SELECT id, title, is_completed FROM todos")
+                .unwrap();
+            let results = stmt.query([]).unwrap().map(|row| {
+                Todo {
+                    id: row.get(0u),
+                    title: row.get(1u),
+                    is_completed: row.get(2u),
+                }
+            }).collect::<Vec<Todo>>();
+
+            let results = results.to_json();
+            response
+                .content_type("json")
+                .send(format!("{}", results));
+        }
+
+        post "/todos" => |request, response| {
+            fn store_todo(todo: Todo) -> String {
+                let conn = PostgresConnection::connect("postgres://rustmvc@localhost", &NoSsl).unwrap();
+
+                conn.execute("INSERT INTO todos (title, is_completed) VALUES ($1, $2)", &[&todo.title, &todo.is_completed]).unwrap();
+
+                json::encode(&todo)
+            }
+
+            let (status, body) = match request.json_as::<Todo>() {
+                Some(t) => (http::status::Created, store_todo(t)),
+                None => (http::status::BadRequest, "{\"error\":\"cannot be parsed\"}".to_string()),
+            };
+
+            response.status_code(status).send(body);
+        }
+    };
+
     server.utilize(StaticFilesHandler::new("frontend/"));
     server.utilize(Nickel::json_body_parser());
     server.utilize(Nickel::query_string());
 
-    server.get("/todos", get_todos);
-    server.post("/todos", post_todo);
+    server.utilize(router);
 
     server.listen(Ipv4Addr(127, 0, 0, 1), 6767);
 }
 
-fn get_todos (request: &Request, response: &mut Response) {
-    let conn = PostgresConnection::connect("postgres://rustmvc@localhost",
-                                           &NoSsl).unwrap();
-
-    let stmt = conn.prepare("SELECT id, title, is_completed FROM todos")
-        .unwrap();
-    let results = stmt.query([]).unwrap().map(|row| {
-        Todo {
-            id: row.get(0u),
-            title: row.get(1u),
-            is_completed: row.get(2u),
-        }
-    }).collect::<Vec<Todo>>();
-
-    let results = results.to_json();
-
-    response
-        .content_type("json")
-        .send(format!("{}", results));
-}
-
-fn post_todo (request: &Request, response: &mut Response) {
-    let (status, body) = match request.json_as::<Todo>() {
-        Some(t) => (http::status::Created, store_todo(t)),
-        None => (http::status::BadRequest, "{\"error\":\"cannot be parsed\"}".to_string()),
-    };
-
-    response.status_code(status).send(body);
-}
-
-fn store_todo(todo: Todo) -> String {
-    let conn = PostgresConnection::connect("postgres://rustmvc@localhost", &NoSsl).unwrap();
-
-    conn.execute("INSERT INTO todos (title, is_completed) VALUES ($1, $2)", &[&todo.title, &todo.is_completed]).unwrap();
-
-    json::encode(&todo)
-}
